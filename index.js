40d37cd54f96809c1146923899b18585a94817aa
yanuaradjinugroho <61837955+yanuaradjinugroho@users.noreply.github.com>
2021-11-13 02:03:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const client = require('./module/connection.js')
+const client = require('./connection.js');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-    console.log(`app started on port ${PORT}`)
+app.listen(port, () => {
+    console.log('Server started at http://localhost:' + port)
 });
-
-var sql = "SELECT fnget_itbx('PENGHIJAUAN', 'Taman Lingkungan', 'Cagar Budaya Bersejarah & Pengetahuan')";
 
 client.connect();
 
-app.get('/itbx', (req, res)=>{
-    client.query(sql, function(err, result) {if(!err){
-            res.send(result.rows);
-        }
-    });
-    client.end;
-  })
+app.get("/", async (req, res) => {
+    let results = {}
+    results.rows = []
+    try {
+        const keterangan_tipekegiatans = req.query.tipekegiatan;
+        const keterangan_kegiatans = req.query.kegiatan;
+        const keterangan_subzonas = req.query.subzona;
+
+        results = await client.query("SELECT izin.keterangan_izin FROM peraturan INNER JOIN tipe_kegiatan on peraturan.kode_tipekegiatan = tipe_kegiatan.kode_tipekegiatan INNER JOIN kegiatan on peraturan.kode_tipekegiatan = kegiatan.kode_tipekegiatan and peraturan.id_kegiatan = kegiatan.id_kegiatan INNER JOIN sub_zona on peraturan.kode_subzona = sub_zona.kode_subzona INNER JOIN zona on sub_zona.kode_zona = zona.kode_zona INNER JOIN izin on peraturan.kode_izin = izin.kode_izin WHERE tipe_kegiatan.keterangan_tipekegiatan = $1 AND kegiatan.keterangan_kegiatan = $2 AND sub_zona.keterangan_subzona = $3",[keterangan_tipekegiatans,keterangan_kegiatans,keterangan_subzonas])
+    }
+    catch (e) {
+        console.log("error")
+    }
+    finally {
+        res.setHeader("content-type", "application/json")
+        res.send(JSON.stringify(results.rows))
+    }
+})
